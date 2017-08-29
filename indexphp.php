ef15acf594f2b324f5e7980bd430cebe87de9d2f
Ankush Gattewar <ankushgattewar@gmail.com>
2017-08-29 09:35:39 +0530

    First Commit

---
+++ indexphp.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>HOME</title>
+    </head>
+    <body>
+        <?php
+       echo "Hello, world! This is my first PHP project!";
+        ?>
+    </body>
+</html>

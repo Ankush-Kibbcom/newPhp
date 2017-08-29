35dd5cde585d5f7390068182e11c4cf25d95149a
Ankush Gattewar <ankushgattewar@gmail.com>
2017-08-29 12:32:02 +0530

    Third updation

--- indexphp.php
+++ indexphp.php
@@ -2,4 +2,14 @@
         $token = openssl_random_pseudo_bytes(64);
         $token = bin2hex($token);
         echo $token;
+		
+		$header = array();
+		$header [] = '';
+		$curl = curl_init();
+		
+		curl_setopt($curl,CURLOPT_URL,'');
+		curl_setopt($curl,CURLOPT_HTTPHEADER,$header);
+		$result = curl_exec($curl);
+		$result = json_decode($result);
+		echo $result;
         ?>
